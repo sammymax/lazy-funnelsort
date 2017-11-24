97fb9cf9f97478ee7247dc2edeb4a5a67e37058c
Philip Sun <philwsun@gmail.com>
2017-11-24 18:21:20 -0500

    Better tests

--- test.cpp
+++ test.cpp
@@ -1,10 +1,29 @@
 #include <functional>
 #include <vector>
+#include <iostream>
 
 #include "funnelsort.h"
 
+using std::cout;
+
 int main() {
-	std::vector<int> v {12, 2};
-	funnelsort(v.begin(), v.end(), std::greater<int>());
-	funnelsort(v.begin(), v.end());
+	for (int i = 1; i < 1000; i++) {
+		std::vector<int> v(i);
+		for (int &x : v) x = rand();
+		auto res = funnelsort(v.begin(), v.end());
+		sort(v.begin(), v.end());
+
+		bool ok = true;
+		for (int j = 0; j < i; j++)
+			if (res[j] != v[j]) ok = false;
+
+		if (!ok) {
+			cout << "failed! ours/theirs:\n";
+			for (auto x : res) cout << x << ' ';
+			cout << '\n';
+			for (auto x : v) cout << x << ' ';
+			cout << '\n';
+			break;
+		}
+	}
 }
