a01e5ce6601ea9fe45c49391ac47b5517dc9c5cf
Uma <uma.roy.us@gmail.com>
2017-12-03 23:56:45 -0500

    Added timing info to test_paramter

--- test_parameter.cpp
+++ test_parameter.cpp
@@ -1,16 +1,24 @@
 #include <functional>
 #include <vector>
 #include <iostream>
+#include <time.h>
+#include <ctime>
+#include <chrono>
 
 #include "funnelsort.h"
 
 using std::cout;
 using std::cin;
 using std::endl;
+using namespace std::chrono;
+
 int main(int argc, char** argv) {
 	int arr_size = atoi(argv[1]);
 	int method = atoi(argv[2]); //0 for funnelsort, 1 for normal
 	//cout << arr_size << endl << method;
+
+	high_resolution_clock::time_point start = high_resolution_clock::now();
+	//clock_t start = clock();
 
 	std::vector<int> v(arr_size);
 	for (int &x : v) x = rand();
@@ -21,7 +29,19 @@ int main(int argc, char** argv) {
 	
 	std::vector<int> res(arr_size);
 	
+	high_resolution_clock::time_point end = high_resolution_clock::now();
+	duration<double> time_span = duration_cast<duration<double>>(end-start);
+
+	cout << "Milliseconds for prep: " << time_span.count() << endl;
 	//cout << "Some output"; 
+	//time_t start_time;
+	//time_t end_time;
+
+	//time(&start_time);
+
+	//start = clock();
+	start = high_resolution_clock::now();
+
 	if (method == 0) {
 		cout << "Using funnelsort" << endl;
 		res = funnelsort(v.begin(), v.end());
@@ -29,4 +49,16 @@ int main(int argc, char** argv) {
 		cout << "Using std sort" << endl;
 		sort(v.begin(), v.end());
 	}
+
+	end = high_resolution_clock::now();
+	time_span = duration_cast<duration<double>>(end-start);
+	cout << "Sort time: " <<  time_span.count() << endl;
+
+	//cout << "Milliseconds: " << clock() - start << endl;
+	//time(&end_time);
+
+	//double seconds = difftime(end_time, start_time);
+	//cout << "Seconds: " << seconds << endl;
+
+
 }
