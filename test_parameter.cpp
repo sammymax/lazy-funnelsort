4f3cad47bba560206ef9ec5af50a4b3fc8288881
Philip Sun <philwsun@gmail.com>
2017-12-13 02:44:15 -0500

    Add code to work with Intel PCM

--- test_parameter.cpp
+++ test_parameter.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 
 #include "funnelsort.h"
+#include "cpucounters.h"
 
 using std::cout;
 using std::cin;
@@ -41,6 +42,16 @@ int main(int argc, char** argv) {
 
 	//start = clock();
 	start = high_resolution_clock::now();
+	PCM * m = PCM::getInstance();
+	m->resetPMU();
+	PCM::ErrorCode returnResult = m->program();
+	if (returnResult != PCM::Success){
+		m->cleanup();
+		std::cerr << "Intel's PCM couldn't start" << std::endl;
+		std::cerr << "Error code: " << returnResult << std::endl;
+		exit(1);
+	}
+	SystemCounterState before_sstate = getSystemCounterState();
 
 	if (method == 0) {
 		cout << "Using funnelsort" << endl;
@@ -51,8 +62,17 @@ int main(int argc, char** argv) {
 	}
 
 	end = high_resolution_clock::now();
+	SystemCounterState after_sstate = getSystemCounterState();
 	time_span = duration_cast<duration<double>>(end-start);
+	m->cleanup();
 	cout << "Sort time: " <<  time_span.count() << endl;
+	cout << "L3 miss ratio: " << getL3CacheHitRatio(before_sstate,after_sstate) << endl;
+	cout << "L3 hits: " << getL3CacheHits(before_sstate,after_sstate) << endl;
+	cout << "L3 miss ratio: " << getL3CacheMisses(before_sstate,after_sstate) << endl;
+
+	cout << "L2 hits: " << getL2CacheHitRatio(before_sstate,after_sstate) << endl;
+	cout << "L2 hits: " << getL2CacheHits(before_sstate,after_sstate) << endl;
+	cout << "L2 misses: " << getL2CacheMisses(before_sstate,after_sstate) << endl;
 
 	//cout << "Milliseconds: " << clock() - start << endl;
 	//time(&end_time);
